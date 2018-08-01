f81289184c25261b29a2a9563efbb0b9fdfaad34
Gideão Silva <gideaoms@gmail.com>
2018-08-01 20:32:25 -0300

    adicionado redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import Chat from './pages/chat'
 
-const App = () => <h1>App</h1>
+const App = () => (
+  <Provider store={store}>
+    <Chat />
+  </Provider>
+)
 
 export default App
