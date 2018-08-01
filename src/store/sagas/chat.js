1aa535848540359147831b18638bf60c37d33dcb
Gideão Silva <gideaoms@gmail.com>
2018-08-01 20:50:47 -0300

    exclui o sagas, pois ele não será necessário

--- src/store/sagas/chat.js
+++
@@ -1,6 +0,0 @@
-import { put } from 'redux-saga/effects'
-import { Creators as ChatActions } from '../ducks/chat'
-
-export function * addMessage (action) {
-  yield put(ChatActions.addMessage(action.payload.message))
-}
--- src/store/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all, takeLatest } from 'redux-saga/effects'
-import { Types as ChatTypes } from '../ducks/chat'
-import { addMessage } from './chat'
-
-export default function * rootSaga () {
-  yield all([
-    takeLatest(ChatTypes.ADD_MESSAGE, addMessage)
-  ])
-}
