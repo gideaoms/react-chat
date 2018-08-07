8b86e5c22cef8e34fdadfc0d5e59e3b9f8b2fa91
Gideão Silva <gideaoms@gmail.com>
2018-08-07 09:14:23 -0300

    estava dando erro no metrics de padding

--- src/components/Presentational/MessageCount/styles.js
+++ src/components/Presentational/MessageCount/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { metrics } from '../../../styles'
+import metrics from '../../../styles/metrics'
 
 export const Container = styled.div`
   display: flex;
